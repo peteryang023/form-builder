7754898b9f78e27bc16c198e5147836c95b6516a
Peter Yang <peteryang@thinkgeo.com>
2018-10-20 23:23:58 +0800

    implemented "edit text properties"

---
+++ demo/formbuilder.js
@@ -0,0 +1,25 @@
+class formbuilder{
+    listControls(){
+
+    }
+
+    static registerControl(control){
+
+    }
+
+   static  unregisterControl(control){
+
+    }
+
+   static  addControl(control){
+
+    }
+
+   static  removeControl(control){
+
+    }
+
+    getHtml(){
+
+    }
+}
